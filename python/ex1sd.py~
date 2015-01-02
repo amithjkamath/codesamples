bf0b218306551529195f1fd8cf49d95ab7ed98de
Amith <kamath.quaero@gmail.com>
2015-01-02 01:32:22 -0500

    Adding python code for learn python the hard way

---
+++ python/ex1sd.py~
@@ -0,0 +1,3 @@
+# This is a comment, and won't do anything really.
+print 'This line is printed'
+print "Double quotes and single quotes are both allowed!"
---
+++ python/ex2sd.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# This is a comment and is ignored.
+# The first line in the program is not a comment. It is a hack to enable text format detection.
+
+print "This line prints!"
+print "This also prints: # text here"
+print "The # in the previous line is part of a string and hence is not indicating any comment: so python reads text after that character"
