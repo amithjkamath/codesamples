43cf3ea6a658458786268ca9facdd95173347986
akamath <amith.kamath@mathworks.com>
2016-02-02 18:09:38 -0500

    renamed testlib to testClib and added a new shared library example

---
+++ cpp/shrlib/libcode.cpp
@@ -0,0 +1,16 @@
+#include "libcode.h"
+
+double add(double a, double b)
+{
+	return a + b;
+}
+
+double minus(double a, double b)
+{
+	return a - b;
+}
+
+double product(double a, double b)
+{
+	return a*b;
+}
---
+++ cpp/shrlib/libcode.h
@@ -0,0 +1,8 @@
+#ifndef LIBCODE_H
+#define LIBCODE_H // done to not include header contents multiple times
+
+double add(double a, double b);
+double minus(double a, double b);
+double product(double a, double b);
+
+#endif
