cfa174fab354accfdca33d48f427f7d93687a10d
akamath <amith.kamath@mathworks.com>
2016-11-03 13:59:27 -0400

    added the MVC version of the flask App

--- python/flasktest/hw.py
+++ python/flasktest/hw.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+# code from http://hplgit.github.io
+# and http://hplgit.github.io/web4sciapps/doc/pub/._web4sa_flask000.html
+
 import sys, math
 r = float(sys.argv[1])
 s = math.sin(r)
