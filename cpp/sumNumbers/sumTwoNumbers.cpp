1a4a134dd7b9a34cc3a47f6786462eaded9d7347
AKAMath <amithjkamath@users.noreply.github.com>
2018-08-17 02:25:44 -0400

    Adding edx algo course code

---
+++ cpp/sumNumbers/sumTwoNumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    int a;
+    int b;
+    std::cin >> a;
+    std::cin >> b;
+
+    std::cout << "Sum of " << a << " and " << b << " is " << a + b << std::endl;
+    return 0;
+}
